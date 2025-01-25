2b13c12b1787cee3f0fc9236907acb1b6bc83eee
ByteMaster69 <amitoj.mehta@gmail.com>
2025-01-25 16:35:15 +0530

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,32 @@
-import { Header } from "@/sections/Header";
-import { Hero } from "@/sections/Hero";
-import { LogoTicker } from "@/sections/LogoTicker";
-import { ProductShowcase } from "@/sections/ProductShowcase";
-import { Pricing } from "@/sections/Pricing";
-import { Testimonials } from "@/sections/Testimonials";
-import { CallToAction } from "@/sections/CallToAction";
-import { Footer } from "@/sections/Footer";
+"use client";
+import CallToAction from "@/sections/CallToAction";
+import Footer from "@/sections/Footer";
+import Header from "@/sections/Header";
+import Hero from "@/sections/Hero";
+import LogoTicker from "@/sections/LogoTicker";
+
+import ProductShowcase from "@/sections/ProductShowcase";
+
+import Lenis from "lenis";
+import { useEffect } from "react";
+import 'lenis/dist/lenis.css'
+import Pricing from "@/sections/Pricing";
+import Testimonials from "@/sections/Testimonials";
+
 
 export default function Home() {
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis();
+
+    // Use requestAnimationFrame to continuously update the scroll
+    function raf(time:number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <>
       <Header />
